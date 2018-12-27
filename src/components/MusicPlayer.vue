cca4f67d21b3c224fa86486251c5612d3f38a6d4
Ekta Chavan <ekta@gethappie.me>
2018-12-26 19:11:05 -0500

    audio player plugged in

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -4,8 +4,9 @@
 
     </div>
     <div class="track-player">
-      <button @click="nextTrack">Next</button>
+      <audio controls :src="currentTrack.url" type="audio/mpeg"/>
       <button @click="prevTrack">Prev</button>
+      <button @click="nextTrack">Next</button>
     </div>
   </div>
 </template>
@@ -23,6 +24,9 @@ export default {
   computed: {
     albumArtStyle () {
       return { 'background-image': `url('${this.$store.state.currentTrack.cover_image}')`, 'background-size': 'cover' }
+    },
+    currentTrack () {
+      return this.$store.state.currentTrack
     }
   },
   methods: {
@@ -40,12 +44,12 @@ export default {
   .player {
     width: 40%;
     height: 70%;
-    background-color: black;
+    background-color: white;;
     display: flex;
     flex-direction: column;
   }
   .album-art {
-    flex-grow: 2;
+    flex-grow: 8;
   }
   .track-player {
     flex-grow: 1;
